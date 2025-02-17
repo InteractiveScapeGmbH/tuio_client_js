a8b1df227bae410eecd47a857fb3f896a31ac35c
Erich Querner <querner@interactive-scape.com>
2025-02-17 14:17:11 +0100

    add necessary exports

--- src/tuio-client.ts
+++ src/tuio-client.ts
@@ -1,2 +1,17 @@
+export { WebsocketTuioReceiver } from "./common/WebsocketTuioReceiver";
 export { Tuio11Client } from "./tuio11/Tuio11Client";
+export type { Tuio11Listener } from "./tuio11/Tuio11Listener";
+export { Tuio11Cursor } from "./tuio11/Tuio11Cursor";
+export { Tuio11Object } from "./tuio11/Tuio11Object";
+export { Tuio11Blob } from "./tuio11/Tuio11Blob";
+
 export { Tuio20Client } from "./tuio20/Tuio20Client";
+export type { Tuio20Listener } from "./tuio20/Tuio20Listener";
+export { Tuio20Object } from "./tuio20/Tuio20Object";
+export { Tuio20Pointer } from "./tuio20/Tuio20Pointer";
+export { Tuio20Token } from "./tuio20/Tuio20Token";
+export { Tuio20Bounds } from "./tuio20/Tuio20Bounds";
+export { Tuio20Symbol } from "./tuio20/Tuio20Symbol";
+export { ScapeXMobile } from "./extensions/ScapeXMobile";
+export type { SxmConfig } from "./extensions/ScapeXMobile";
+export type { SxmMessage } from "./extensions/ScapeXMobile";
