ecc31739e226a0e71a99223905b2b589d90aaf4c
Erich Querner <querner@interactive-scape.com>
2025-02-17 14:18:43 +0100

    just some whitespace  reformat

--- src/extensions/ScapeXMobile.ts
+++ src/extensions/ScapeXMobile.ts
@@ -1,9 +1,9 @@
-export interface SxmConfig{
+export interface SxmConfig {
     roomId: string;
     mqttUrl: string;
 }
 
-interface SxmMessage{
+export interface SxmMessage {
     type: string;
     topic: string;
     value: string;
@@ -11,10 +11,10 @@ interface SxmMessage{
 
 type SxmConfigEvent = (config: SxmConfig) => void;
 
-export class ScapeXMobile{
+export class ScapeXMobile {
     private _ws: WebSocket | null;
     private readonly _scapeEngineAddress: string;
-    private readonly _scapeEnginePort :number = 3330;
+    private readonly _scapeEnginePort: number = 3330;
     private _sxmConfig: SxmConfig;
     private _eventHandlers: SxmConfigEvent[] = [];
     private _defaultMqttUrl: string = "broker.hivemq.com"
@@ -22,38 +22,38 @@ export class ScapeXMobile{
     constructor(scapeEngineAddress: string) {
         this._ws = null;
         this._scapeEngineAddress = scapeEngineAddress;
-        this._sxmConfig = {roomId: "", mqttUrl: ""};
+        this._sxmConfig = { roomId: "", mqttUrl: "" };
     }
 
-    public set onConfigChange(handler: SxmConfigEvent){
+    public set onConfigChange(handler: SxmConfigEvent) {
         this._eventHandlers.push(handler);
     }
 
-    public connect(){
+    public connect() {
         this._ws = new WebSocket(`ws://${this._scapeEngineAddress}:${this._scapeEnginePort}`);
         this._ws.onmessage = (message) => this.parseSxmConfig(message);
-        this._ws.onclose = (message) => {console.log(message);};
+        this._ws.onclose = (message) => { console.log(message); };
     }
 
-    private parseSxmConfig(message: MessageEvent){
+    private parseSxmConfig(message: MessageEvent) {
         const sxmMessage: SxmMessage = JSON.parse(message.data) as SxmMessage;
         console.log(sxmMessage)
-        switch (sxmMessage.type){
-            case "server":{
+        switch (sxmMessage.type) {
+            case "server": {
                 this._sxmConfig.mqttUrl = sxmMessage.value === "" ? this._defaultMqttUrl : sxmMessage.value;
                 break;
             }
-            case "subscribe":{
+            case "subscribe": {
                 this._sxmConfig.roomId = sxmMessage.topic.split("/")[0];
                 break;
             }
         }
-        this._eventHandlers.forEach((handler)=>{
+        this._eventHandlers.forEach((handler) => {
             handler(this._sxmConfig);
         })
     }
 
-    public disconnect(){
+    public disconnect() {
         this._ws?.close();
     }
 }
